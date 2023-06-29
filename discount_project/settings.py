f018c23e9cb11ff26375e03f8c2c6f1613f5dc6a
andreysidorovv <sidorov.andrey2005@mail.ru>
2023-06-29 20:48:10 +0300

    for_discord?

--- discount_project/settings.py
+++ discount_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2w5lht1uy9ee-rve3kh7tm1xkq9xs17a$r(^_f28#(!6!c@tc6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['discounts.std-2364.ist.mospolytech.ru']
+ALLOWED_HOSTS = []
 
 
 # Application definition
